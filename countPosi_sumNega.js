ae689919ec3fdf6a1df7bd2a304bc2cb5552173c
Sarpscode <sarponge60@gmail.com>
2023-11-20 20:20:50 +0000

    countOfPositive_sumOfNegative

---
+++ countPosi_sumNega.js
@@ -0,0 +1,27 @@
+
+function countPositivesSumNegatives(input) {
+
+    if(input === null){
+      return[]
+    }
+    //if (input == null || input.length == 0) {
+        //return [];
+    //}   
+
+    if(input.filter(number => number != 0).length == 0) return [];
+    
+    let countOfPositives = 0;
+    let sumOfNegatives = 0;
+    
+    for(let number of input){
+      if(number < 0){
+        sumOfNegatives = sumOfNegatives + number;
+      }else if(number > 0){
+        countOfPositives = countOfPositives + 1;
+      }
+    }
+    
+    return [countOfPositives, sumOfNegatives]
+  }
+
+  
