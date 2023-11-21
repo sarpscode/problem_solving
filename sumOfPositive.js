a3321b8e7ebcc2645553df37cb5006c1039dbd4c
Sarpscode <sarponge60@gmail.com>
2023-11-21 11:51:37 +0000

    sumOfPositive

---
+++ sumOfPositive.js
@@ -0,0 +1,11 @@
+function positiveSum(arr) {
+    let sumOfPositives = 0;
+    
+    for(number of arr){
+      if(number >= 0){
+        sumOfPositives = sumOfPositives + number
+      }
+    }
+    return sumOfPositives
+  }
+  
